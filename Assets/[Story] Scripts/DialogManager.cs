45fbae6b7da4546fd0c84087a7c2dd0a48a80227
lichengz <l345zhan@edu.uwaterloo.ca>
2019-01-03 19:35:35 -0500

    dialog system

---
+++ Assets/[Story] Scripts/DialogManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    private bool readyToNextSentence;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && readyToNextSentence)
+        {
+            NextSentence();
+        }
+        if (textDisplay.text == sentences[index])
+        {
+            readyToNextSentence = true;
+        }
+    }
+    IEnumerator Type()
+    {
+        readyToNextSentence = false;
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            readyToNextSentence = false;
+        }
+    }
+}
